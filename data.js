824f608d2a9b84a4a54a5da1b182a75fa6995dce
73597 <108709716+73597@users.noreply.github.com>
2022-07-04 22:37:23 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,624 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1jardn-de-la-unin",
+      "name": "1.Jardín de la Unión",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2816,
+      "initialViewParameters": {
+        "yaw": 1.457310461707161,
+        "pitch": -0.0594547005137418,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6221030162331802,
+          "pitch": -0.4370301586033172,
+          "rotation": 0,
+          "target": "1-2jardn-de-la-unin"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5537770220363321,
+          "pitch": -0.2893255448073937,
+          "title": "Title",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">El Jardín de la Unión&nbsp; es el corazón y el alma de la ciudad de Guanajuato y el hogar de los mejores bares y restaurantes de la ciudad.</font></font><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2jardn-de-la-unin",
+      "name": "2.Jardín de la Unión",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.8549438171326198,
+        "pitch": 0.011890940102748715,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1446443376033315,
+          "pitch": -0.3768703506286091,
+          "rotation": 0,
+          "target": "2-3callejn-del-beso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3callejn-del-beso",
+      "name": "3.Callejón del Beso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -3.0983600720228335,
+        "pitch": -0.668865380779458,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.49743609414017,
+          "pitch": -0.6797059712931528,
+          "rotation": 0,
+          "target": "3-4callejn-del-beso"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1759284023521817,
+          "pitch": -0.6380044066974975,
+          "title": "Title",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Cuenta la leyenda que si tu visita el Callejón del Beso tienes que darle un beso a tu pareja subiendo hasta el tercer escalón de la pared más angosta del callejón para así tener 15 años de prosperidad y amor.</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4callejn-del-beso",
+      "name": "4.Callejón del Beso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2750,
+      "initialViewParameters": {
+        "yaw": -3.064477053354091,
+        "pitch": 0.062427435539410325,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.919398157740746,
+          "pitch": -0.35206455445902485,
+          "rotation": 0,
+          "target": "4-5monumento-al-pipila"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-5monumento-al-pipila",
+      "name": "5.Monumento al Pipila",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -0.8666460149983006,
+        "pitch": -0.5210424485381608,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2394122204338487,
+          "pitch": -0.49969229430233675,
+          "rotation": 0,
+          "target": "5-6monumento-al-pipila"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9819563232227093,
+          "pitch": -0.5573493631591422,
+          "title": "Title",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Monumento al Pípila Héroe, popular de la Insurgencia en 1940 en honor a Juan José Martínez Amaro también conocido&nbsp;</font></font><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">&nbsp;Como \"El Pípila\" por su heroica participación en la independencia de México durante la toma de la Alhóndiga de Granaditas en Guanajuato.</font></font></font></font></div><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></font></font></div><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "5-6monumento-al-pipila",
+      "name": "6.Monumento al Pipila",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.03149842432434369,
+        "pitch": 0.5026904378994708,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6208233010415913,
+          "pitch": -0.0029261207965465275,
+          "rotation": 0,
+          "target": "6-7alhndiga-de-granaditas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7alhndiga-de-granaditas",
+      "name": "7.Alhóndiga de Granaditas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 2.4473943077819698,
+        "pitch": -0.31962452632510896,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0257366244401176,
+          "pitch": -0.5127177432398735,
+          "rotation": 0,
+          "target": "7-8alhndiga-de-granaditas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.651671191208803,
+          "pitch": -0.13826073088748814,
+          "title": "Title",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Debido a su importancia histórica desde 1958 es el Museo Regional de Guanajuato \"Alhóndiga de Granaditas\" donde se encuentra un acervo de más de 9,200 objetos con valor histórico.</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "7-8alhndiga-de-granaditas",
+      "name": "8.Alhóndiga de Granaditas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.9778139475919563,
+        "pitch": -0.24673700713197633,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.941237745383585,
+          "pitch": -0.7122452021942483,
+          "rotation": 0,
+          "target": "8-9tneles-subterrneos-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-9tneles-subterrneos-",
+      "name": "9.Túneles Subterráneos ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2750,
+      "initialViewParameters": {
+        "yaw": 1.4854543218075698,
+        "pitch": 0.08026384569353695,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.096946113730608,
+          "pitch": -0.3137530859675213,
+          "rotation": 0,
+          "target": "9-10tneles-subterrneos"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.821073242339871,
+          "pitch": 0.1800971949528165,
+          "title": "Title",
+          "text": "<div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Los túneles de Guanajuato son famosos porque desde su creación fueron una obra de la ingeniería, nacieron con la idea de proteger a los habitantes de las múltiples inundaciones que ocurrieron en aquella ciudad que había sido construida a la orilla de una&nbsp; cañada.&nbsp;</font></font></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "9-10tneles-subterrneos",
+      "name": "10.Túneles Subterráneos",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 0.023541246352186462,
+        "pitch": -0.0148636751284279,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5847313892981933,
+          "pitch": -0.45951681206809347,
+          "rotation": 0,
+          "target": "10-11festival-cervantino"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11festival-cervantino",
+      "name": "11.Festival Cervantino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": -0.027064747844050885,
+        "pitch": -0.05350923046235323,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6854596786467475,
+          "pitch": -0.4472593568063026,
+          "rotation": 0,
+          "target": "11-12festival-cervantino"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.783079010926663,
+          "pitch": -0.016138272354664807,
+          "title": "Title",
+          "text": "<div>El Festival Internacional Cervantino, además de los espectáculos y exposiciones artísticas, ofrece diversos talleres que van desde la pintura, serigrafía, la producción musical aplicada al rap, graffiti y hip hop hasta temas de tecnología y cultura digital para creadores escénicos.<br></div>"
+        }
+      ]
+    },
+    {
+      "id": "11-12festival-cervantino",
+      "name": "12.Festival Cervantino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.20619653447138475,
+        "pitch": -0.324252025762501,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8640050470482858,
+          "pitch": -0.6820312564493953,
+          "rotation": 0,
+          "target": "12-13festival-del-globo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13festival-del-globo",
+      "name": "13.Festival del Globo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.7182567697074038,
+        "pitch": 0.15755495636137695,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4712427702314654,
+          "pitch": -0.23872360212970456,
+          "rotation": 0,
+          "target": "13-14festival-del-globo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20651233657855883,
+          "pitch": -0.19116334281524594,
+          "title": "Title",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">El Festival Internacional del Globo (FIG) es un evento que se realiza en la ciudad de León en el mes de noviembre y que logra reunir globos aerostáticos de diferentes formas, tamaño y personajes reconocidos con una gran variedad de colores que iluminan el cielo a lo largo de tres dias.</font></font><br><div><font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\"><br></font></font></div>"
+        }
+      ]
+    },
+    {
+      "id": "13-14festival-del-globo",
+      "name": "14.Festival del Globo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -1.7822611359101685,
+        "pitch": 0.2437642721063007,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.107086500497017,
+          "pitch": -0.15708458920527235,
+          "rotation": 0,
+          "target": "0-1jardn-de-la-unin"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Guanajuato-Guanajuato",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
